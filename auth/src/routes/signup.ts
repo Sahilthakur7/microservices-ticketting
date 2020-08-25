c430db8ac8c343a8ac1c79f13646ec30070269c9
sahilthakur7 <sahilrd07@gmail.com>
2020-08-25 21:25:45 +0530

    Singup route created

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Mohit is the user");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Please enter valid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Please enter a password between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    res.send("Mohit is the user");
+  }
+);
 
 export { router as signupRouter };
