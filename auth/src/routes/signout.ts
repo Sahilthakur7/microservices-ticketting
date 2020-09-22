c754993615fad43b19ee62eded673168a8b996f7
sahilthakur7 <sahilrd07@gmail.com>
2020-09-22 12:00:54 +0530

    Implemented current-user and signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Mohit is the user");
+  req.session = null;
+  return res.send({});
 });
 
 export { router as signoutRouter };
